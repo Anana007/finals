83e76479f2ed6e12df429990434dc322b55755e4
Ana Lakić <ana.lakic1408@gmail.com>
2023-06-30 20:20:29 +0200

    parcijalni_reactJS_

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import User from "./User";
+import { useState } from "react";
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(`https://api.github.com/users/${inputValue}`);
+      if (response.ok) {
+        const userData = await response.json();
+        setUsers([userData]); 
+        console.log(userData);
+      } else {
+        console.error("Failed to fetch user data");
+      }
+    } catch (error) {
+      console.error("Error occurred while fetching user data", error);
+    }
+  };
+
+  const handleButtonClick = () => {
+    if (inputValue) {
+      fetchData();
+      setInputValue("");
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <h1>GitHub username:</h1>
+        <input
+          type="text"
+          placeholder="Unesi korisničko ime"
+          onChange={(e) => setInputValue(e.target.value)}
+          value={inputValue}
+        ></input>
+        <hr></hr>
+        <button onClick={handleButtonClick}>GO !</button>
+        {users.map(user => (
+          <User user={user} key={user.id} /> 
+        ))}
+      </div>
+    </>
   );
 }
 
